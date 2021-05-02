4a165e70203984bef918e9265eed438423ecabcb
SinJungho <zmtls0516@naver.com>
2021-05-02 15:48:25 +0900

    이력서 본문 파트 마다 크기 조절 및 프로젝트 링크 연결

---
+++ event.js
@@ -0,0 +1,11 @@
+function zoomIn(event) {
+    event.target.style.width = "600px";
+    event.target.style.height = "336px";
+    event.target.style.transition = "all 0.5s";
+  }
+
+  function zoomOut(event) {
+    event.target.style.width = "500px";
+    event.target.style.height = "280px";
+    event.target.style.transition = "all 0.5s";
+  }
